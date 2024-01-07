11c016b00b1ebf81378d10ddcf613975ef5b14a4
dpwgc <281148314@qq.com>
2024-01-07 22:42:22 +0800

    add http client

--- http.go
+++ http.go
@@ -16,35 +16,35 @@ const (
 	MethodDELETE  = "DELETE"
 )
 
-func GET(url string, header ...Params) (int, Data, error) {
+func GET(url string, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodGET, url, nil, header...)
 }
 
-func HEAD(url string, header ...Params) (int, Data, error) {
+func HEAD(url string, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodHEAD, url, nil, header...)
 }
 
-func OPTIONS(url string, header ...Params) (int, Data, error) {
+func OPTIONS(url string, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodOPTIONS, url, nil, header...)
 }
 
-func POST(url string, body Data, header ...Params) (int, Data, error) {
+func POST(url string, body []byte, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodPOST, url, body, header...)
 }
 
-func PUT(url string, body Data, header ...Params) (int, Data, error) {
+func PUT(url string, body []byte, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodPUT, url, body, header...)
 }
 
-func PATCH(url string, body Data, header ...Params) (int, Data, error) {
+func PATCH(url string, body []byte, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodPATCH, url, body, header...)
 }
 
-func DELETE(url string, header ...Params) (int, Data, error) {
+func DELETE(url string, header ...map[string]string) (int, Data, error) {
 	return HTTP(MethodDELETE, url, nil, header...)
 }
 
-func HTTP(method, url string, body Data, header ...Params) (int, Data, error) {
+func HTTP(method, url string, body []byte, header ...map[string]string) (int, Data, error) {
 	request, err := http.NewRequest(method, url, bytes.NewReader(body))
 	if err != nil {
 		return 0, nil, err
